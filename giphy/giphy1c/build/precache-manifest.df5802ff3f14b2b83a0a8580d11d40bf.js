69858f0126558afe095b670d6808b22dc1ad34b8
flosssolis <60480549+flosssolis@users.noreply.github.com>
2020-08-08 01:03:36 +0300

    two versions of giphy-clone

---
+++ giphy/giphy1c/build/precache-manifest.df5802ff3f14b2b83a0a8580d11d40bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d505296598a87cbe6421020b6c2faee6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de8b9672b03e48057caa",
+    "url": "./static/css/main.bd950d07.chunk.css"
+  },
+  {
+    "revision": "49345c7e064dfafaad99",
+    "url": "./static/js/2.61857b0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.61857b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8b9672b03e48057caa",
+    "url": "./static/js/main.6a0c5b99.chunk.js"
+  },
+  {
+    "revision": "ff8700a86c5511314c12",
+    "url": "./static/js/runtime-main.9f7e5159.js"
+  }
+]);
---
+++ giphy/giphy2f/build/precache-manifest.774e106f9b66cd55af970dd6e8957485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563644b558dc76bb695aacd86341bfb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a277020cb4b9bceb7213",
+    "url": "./static/css/main.063c4da4.chunk.css"
+  },
+  {
+    "revision": "737fce804e9d397516c5",
+    "url": "./static/js/2.be0badb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.be0badb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a277020cb4b9bceb7213",
+    "url": "./static/js/main.5c00fb9c.chunk.js"
+  },
+  {
+    "revision": "2aa62d904b7b6538fd59",
+    "url": "./static/js/runtime-main.9955ebd0.js"
+  }
+]);
