7778e9d38bb237fb1aba98dbe49e86092f655fa6
flosssolis <60480549+flosssolis@users.noreply.github.com>
2020-09-01 13:59:14 +0300

    Add files via upload

---
+++ weather/weather/build/precache-manifest.566050f0812598283ff5f9bc2f0f5f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f512a3008f55db8162ea744d5307963",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3be92d04d28465605f7a",
+    "url": "./static/css/main.86f2eec6.chunk.css"
+  },
+  {
+    "revision": "937794add7a8387aace8",
+    "url": "./static/js/2.a64904d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a64904d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be92d04d28465605f7a",
+    "url": "./static/js/main.598b58af.chunk.js"
+  },
+  {
+    "revision": "7185123dbfbf19a73c74",
+    "url": "./static/js/runtime-main.3def4396.js"
+  }
+]);
